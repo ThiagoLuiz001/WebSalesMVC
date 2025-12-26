f06b018343c488d6018b254db26686696478ffa6
ThiagoLuiz001 <thiagoluizgncalvesdefaria@gmail.com>
2025-12-26 17:20:06 -0300

    Primeiro Commit

---
+++ MVCSaller/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCSaller.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
